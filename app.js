5f51cd3a81497e40cd3fc9b04aa4fe8ac74955e2
Moldoveanu Alexandra <moldoveanu.alexandra@gmail.com>
2022-05-01 16:55:22 +0300

    basic functionality and interface

---
+++ app.js
@@ -0,0 +1,83 @@
+let userInput = document.querySelector(".user-input");
+let calculatorButtons = document.querySelectorAll(".calculator-button");
+let operatorButtons = document.querySelectorAll(".operator-button");
+let clearButton = document.querySelector(".clear-button");
+let equalButton = document.querySelector(".equal-button"); 
+let operand1;
+let operand2;
+let operator;
+let result;
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+const operate = (operatorString, num1, num2) => {
+    num1 = parseInt(num1, 10);
+    num2 = parseInt(num2, 10);
+    switch (operatorString) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
+const isOperatorActive = () => {
+    
+    for(let i = 0; i < operatorButtons.length; i++){
+        
+        if(operatorButtons[i].style.backgroundColor === "rgb(237, 145, 33)"){
+            return true;
+        }
+    }
+    return false;
+}
+const displayUserInput = (event) => {
+    userInput.innerText += event.target.innerText;
+    if(isOperatorActive()){
+        clearAll();
+        userInput.innerText += event.target.innerText;
+    }  
+    
+}
+
+const clearAll = (event) => {
+    userInput.innerText = "";
+    operatorButtons.forEach(button => button.style.backgroundColor = "teal");
+}
+
+const setCalcValues = (event) => {
+    operand1 = userInput.innerText;
+    operator = event.target.innerText;
+    event.target.style.backgroundColor = "rgb(237,145,33)";
+   
+    console.log(operand1);
+    console.log(operator);
+}
+
+showResult = (event) => {
+    
+    operand2 = userInput.innerText;
+    result = operate(operator, operand1, operand2);
+    userInput.innerText = result;
+}
+
+calculatorButtons.forEach( button =>{
+    button.addEventListener("click", displayUserInput);
+}) 
+operatorButtons.forEach( button => {
+    button.addEventListener("click", setCalcValues);
+})
+
+clearButton.addEventListener("click", clearAll);
+equalButton.addEventListener("click", showResult);
+
+
